532612f10a965ebcea8850c7e89c369422f03f14
Pete Gallagher <pete@pjgcreations.co.uk>
2020-12-09 11:24:53 +0000

    Added Code to Flash LEDs branch

--- robot_firmware/Program.cs
+++ robot_firmware/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Device.Gpio;
+using System.Threading;
 
 namespace robot_firmware
 {
@@ -8,6 +10,29 @@ namespace robot_firmware
         {
 
             Console.WriteLine("Hello World!");
+
+            GpioController controller = new GpioController(PinNumberingScheme.Board);
+
+            var pin = 10;
+            var lightTime = 300;
+
+            controller.OpenPin(pin, PinMode.Output);
+
+            try
+            {
+                while(true)
+                {
+                    controller.Write(pin, PinValue.High);
+                    Thread.Sleep(lightTime);
+                    controller.Write(pin, PinValue.Low);
+                    Thread.Sleep(lightTime);
+                }
+            }
+            finally
+            {
+                controller.ClosePin(pin);
+            }
+
         }      
     }
 }
