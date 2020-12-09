73f8ae3ffe630db0c41ac16e687dcaf5e4f730e0
Pete Gallagher <pete@pjgcreations.co.uk>
2020-12-09 11:30:25 +0000

    Added Servo Move Code

--- robot_firmware/Program.cs
+++ robot_firmware/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Device.Gpio;
 using System.Threading;
+using System.Device.Pwm;
+using Iot.Device.ServoMotor;
 
 namespace robot_firmware
 {
@@ -11,32 +13,47 @@ namespace robot_firmware
 
             Console.WriteLine("Hello World!");
 
-            GpioController controller = new GpioController(PinNumberingScheme.Board);
-            var pin = 10;
-            var buttonPin = 26;
+            using PwmChannel pwmChannel1 = PwmChannel.Create(0, 0, 50);
+            using ServoMotor servoMotor1 = new ServoMotor(
+                pwmChannel1,
+                180,
+                700,
+                2400);
 
-            controller.OpenPin(pin, PinMode.Output);
-            controller.OpenPin(buttonPin, PinMode.InputPullUp);            
+            using PwmChannel pwmChannel2 = PwmChannel.Create(0, 1, 50);
+            using ServoMotor servoMotor2 = new ServoMotor(
+                pwmChannel2,
+                180,
+                700,
+                2400);
+
+            servoMotor1.Start();
+            servoMotor2.Start();
 
             try
             {
-                while (true)
+                while(true)
                 {
-                    if (controller.Read(buttonPin) == false)
-                    {
-                        controller.Write(pin, PinValue.High);
-                    }
-                    else
-                    {
-                        controller.Write(pin, PinValue.Low);
-                    }
+                    MoveToAngle(servoMotor1, 70);
+                    Thread.Sleep(2000);
+                    MoveToAngle(servoMotor2, 20);
+                    Thread.Sleep(2000);
+                    MoveToAngle(servoMotor1, 150);
+                    Thread.Sleep(2000);
+                    MoveToAngle(servoMotor2, 50);
+                    Thread.Sleep(2000);
                 }
             }
             finally
             {
-                controller.ClosePin(pin);
+                servoMotor1.Stop();
+                servoMotor2.Stop();
             }
 
-        }      
+        }
+
+        static void MoveToAngle(ServoMotor Servo, int Angle) {
+            Servo.WriteAngle(Angle);            
+        }
     }
 }
