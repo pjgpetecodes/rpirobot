87cf109290515dc4583a607605fea35c268e35ea
Pete Gallagher <pete@pjgcreations.co.uk>
2020-12-09 14:53:27 +0000

    Used Hosted for Blazor App

--- robot_web/Client/Program.cs
+++ robot_web/Client/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace robot_web
+namespace robot_web.Client
 {
     public class Program
     {
