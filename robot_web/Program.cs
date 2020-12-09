195bd3301b19323e587a36a307ab882392bc9dc9
Pete Gallagher <pete@pjgcreations.co.uk>
2020-12-09 09:10:44 +0000

    Create Blazor Wasm Project

--- robot_web/Program.cs
+++ robot_web/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace robot_web.Client
+namespace robot_web
 {
     public class Program
     {
